c34d3aba11f126126ba31a96730413358ee8df51
mygapix <39927014+mygapix@users.noreply.github.com>
2019-04-24 11:50:46 +0200

    bulma carousel fail

--- src/components/architecture.js
+++ src/components/architecture.js
@@ -1,36 +1,60 @@
 import React from 'react';
 import './style.scss';
 import bulmaCarousel from 'bulma-carousel';
-import Helmet from 'react-helmet';
 import img1 from '../images/arch/a1.jpg';
+import img2 from '../images/arch/a2.jpg';
+import img3 from '../images/arch/a3.jpg';
+import img4 from '../images/arch/a4.jpg';
+import img5 from '../images/arch/a5.jpg';
+
 
 export default class Architecture extends React.Component {
 	constructor(props) {
 		super(props);
 		console.log('constructor');
-
 	}
+
 	componentDidMount() {
 		console.log('component did mount');
-		const carousel = bulmaCarousel.attach('#carousel-demo', {});
+		const carousel = bulmaCarousel.attach('#carousel-demo', {
+			slidesToScroll: 1,
+			slidesToShow: 2,
+			loop: true,
+			infinite: true,
+			
+		});
 		console.log('carousel', carousel);
 	}
 	render() {
 		console.log('render');
 		return (
-			<div id="carousel-demo" className="carousel">
-				<div className="img-wrapper item-1">
-					<img src={img1}/>
-					<p className="img-description">Opis</p>
+			<div className="container-arch">
+				<div className="title is-1">
+					<h4>ARCHITECTURE</h4>
 				</div>
-				<div className="img-wrapper item-2">
-					<img src={img1}/>
-				</div>
-				<div className="img-wrapper item-3">
-					<img src={img1}/>
-				</div>
+					<div id="carousel-demo" className="carousel" >
+			 			<div className="img-wrapper item-1">
+							<img src={img1}/>
+							<p className="img-description">Opis</p>
+						</div>
+						<div className="img-wrapper item-2">
+							<img src={img2}/>
+							<p className="img-description">Opis vasgregerg</p>
+						</div>
+						<div className="img-wrapper item-3">
+							<img src={img3}/>
+							<p className="img-description">Opis dfb  dfbbd b dfbdfbfdb dfdffb</p>
+						</div>
+						<div className="img-wrapper item-4">
+							<img src={img4}/>
+							<p className="img-description">Opis dfbdf dfbd jyrtshb erggr wefvedvdfrgvreg  thr</p>
+				  	</div>
+						<div className="img-wrapper item-5">
+							<img src={img5}/>
+							<p className="img-description">Opis</p>
+				  	</div>
+					</div>
 			</div>
 		);
 	}
 }
-			
--- src/components/helmet.js
+++ src/components/helmet.js
@@ -23,13 +23,15 @@ export default () => (
 					name="viewport"
 					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
 				/>
-				<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-				 integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
-				 crossorigin="anonymous"
+				<link rel="stylesheet"
+					href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+				 	integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+				 	crossorigin="anonymous"
 				/>
-				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-				 integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-				 crossorigin="anonymous"
+				<link rel="stylesheet"
+					href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+				 	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+				 	crossorigin="anonymous"
 				/>
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"/>
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-carousel@4.0.4/dist/css/bulma-carousel.min.css"/>
